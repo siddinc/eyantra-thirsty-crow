349614cf7b9baf58a54f2672fbaa5bd2aa633537
Vikrant Gajria <vikrantgajria@gmail.com>
2018-12-26 16:57:00 +0530

    Add 3.1A

--- three/Task 3.1A/Code/echo.py
+++ three/Task 3.1A/Code/echo.py
@@ -5,14 +5,12 @@ PORT = "COM5"
 MAC_PORT = r"/dev/tty.SLAB_USBtoUART"
 
 if __name__ == "__main__":
-    ser = serial.Serial("/dev/tty.SLAB_USBtoUART", 9600, timeout=0.005)
-    while True:
-        user_input = input("Enter key: ")
-        ########## ENTER YOUR CODE HERE ############
-
-        ser.write(user_input.encode())
-
-        res = ser.readline()
-        print(res)
-
-        ############################################
+    with serial.Serial(MAC_PORT, 9600, timeout=0.005) as ser:
+        ser.flushInput()
+        ser.flushOutput()
+        while True:
+            user_input = input("Enter key: ")
+            ser.write(user_input.encode())
+            time.sleep(0.1)
+            line = ser.readline()
+            print(line)
